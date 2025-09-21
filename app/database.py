00e0039c7283892ff9149dcedf95817c0a806599
p7raneeth <praneethkrishna27@hotmail.com>
2025-09-21 20:48:08 +0530

    added docker file and modified database url to docker internal

--- app/database.py
+++ app/database.py
@@ -1,7 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:mYsecurEpassworD@localhost/alembic_db2"
+# SQLALCHEMY_DATABASE_URL = "postgresql://postgres:mYsecurEpassworD@localhost/alembic_db2"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:mYsecurEpassworD@host.docker.internal:5432/alembic_db2"
+
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
